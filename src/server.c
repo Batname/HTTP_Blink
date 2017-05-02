0780ac59a78b6ca86b4f1b6118596fa6b1d4f90b
Batname <dadubinin@gmail.com>
2017-05-02 21:26:01 +0000

    add ip config

--- src/server.c
+++ src/server.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,7 +12,7 @@
 
 int main(int argc, const char *argv[])
 {
-    int sockfd, new_sockfd, yes = 1;
+    int sockfd, new_sockfd, status, yes = 1;
 
     // Address information
     struct sockaddr_in host_addr, client_addr;
@@ -21,6 +23,14 @@ int main(int argc, const char *argv[])
 
     sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) fatal("in socket");
+    
+    status = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    if (status == -1) fatal("setting socket option SO_REUSEADDR");
+
+    host_addr.sin_family = AF_INET;                 // Host byte order
+    host_addr.sin_port = htons(PORT);               // Short network byte order
+    host_addr.sin_addr.s_addr = INADDR_ANY;         // Auto fill my IP
+    memset(&(host_addr.sin_zero), '\0', 8);         // Zero the rest of the struct
 
     return 0;
 }
