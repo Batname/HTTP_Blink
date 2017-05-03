7bf6eb81fe969527c6c0aa24debbb84460c696fe
bat <dadubinin@gmail.com>
2017-05-03 15:51:07 -0400

    add utils

--- src/utils.c
+++ src/utils.c
@@ -15,10 +15,50 @@ void fatal(char *message)
 
 int send_string(int sockfd, unsigned char *buffer)
 {
-    return 1;
+    int sent_bytes, bytes_to_send;
+    bytes_to_send = strlen(buffer);
+    
+    while(bytes_to_send > 0)
+    {
+        sent_bytes = send(sockfd, buffer, bytes_to_send, 0);
+        if (sent_bytes == -1)
+            return 0; // sent error
+
+        bytes_to_send -= sent_bytes;
+        buffer += sent_bytes;
+    }
+
+    return 1; // return 1 on success
 }
 
 int recv_line(int sockfd, unsigned char *dest_buffer)
 {
-    return 1;
+#define EOL "\r\n" // End-Of-Line byte sequence
+#define EOL_SIZE 2
+
+    unsigned char *ptr;
+    int eol_matched = 0;
+
+    ptr = dest_buffer;
+
+    while ( recv(sockfd, ptr, 1, 0) == 1 ) // read single byte
+    {
+        if(*ptr == EOL[eol_matched])
+        {
+            eol_matched++;
+            if (eol_matched == EOL_SIZE) // if all bytes match terminator
+            {
+                *(ptr + 1 - EOL_SIZE) = '\0'; // terminate the string
+                return strlen(dest_buffer);
+            }
+        }
+        else
+        {
+            eol_matched = 0;
+        }
+        ptr++;
+    }
+
+    return 0; // did not find
 }
+
